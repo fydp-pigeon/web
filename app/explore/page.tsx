6060b7d82573f8dc49dd1d653bc66211b38186df
nbeilis <nicole.beilis@gmail.com>
2023-07-24 12:59:06 -0400

    update explore page

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,12 +1,19 @@
-import { DatasetLink } from '@/_components/landing-page/browse-datasets/DatasetLink';
-
 export default function Explore() {
   return (
-    <div className="flex w-full flex-col items-center">
-      <DatasetLink href="/">Top 5 Popular Datasets</DatasetLink>
-      <div className="grid w-2/3 grid-cols-1 gap-6 md:grid-cols-2">
-        <DatasetLink href="/"></DatasetLink>
-        <DatasetLink href="/"></DatasetLink>
+
+    <div className="grid grid-cols-1 p-16">
+      {/* <DatasetLink href="/">Top 5 Popular Datasets</DatasetLink> */}
+      <div className="flex flex-col h-48 rounded-lg bg-slate-300 text-black">
+        <div className="p-8">
+          <h1>Top 5 Popular Datasets</h1>
+          <h3>1. abc</h3>
+          <h3>2. def</h3>
+          <h3>3. ghi</h3>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-6 gap-4 pt-4">
+        <div className="flex h-64 rounded-lg bg-slate-300 text-primary-content"></div>
+        <div className="flex h-64 rounded-lg bg-slate-300 text-primary-content"></div>
       </div>
     </div>
   );
