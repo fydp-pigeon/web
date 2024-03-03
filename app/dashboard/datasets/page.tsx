f665cab1a30a2f4c81734c40e6c567fca0d57311
Ralph Rouhana <ralph.rouhana@gmail.com>
2024-03-03 15:46:00 -0500

    Remove unnessesary improts

--- app/dashboard/datasets/page.tsx
+++ app/dashboard/datasets/page.tsx
@@ -1,8 +1,5 @@
-import prisma from '@/_lib/server/prismadb';
 import { SearchField } from '../_components/SearchField';
 import { PageHeader } from '../_components/PageHeader';
-import { getServerUser } from '@/_lib/server/getServerUser';
-import { DatasetCard } from './_components/DatasetCard';
 import { Suspense } from 'react';
 import { Spinner } from '@/_components/Spinner';
 import { DatasetsWrapper } from './_components/DatasetsWrapper';
